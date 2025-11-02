01fcedfd82588fac86d4b388d6b3149963bf7f19
aaaa24 <chernov.arseniy@yandex.ru>
2025-11-02 20:59:31 +0300

    added minimal parameter checking

--- chernov.arseniy/P3/main.cpp
+++ chernov.arseniy/P3/main.cpp
@@ -1,2 +1,16 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char ** argv)
+{
+  if (argc < 4) {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  } else if (argc > 4) {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  } else if (argv[1][0] != '1' && argv[1][0] != '2') {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+}
