63269c6f72f0370e3ab1580646046a769720965b
vadik <vadik@debian>
2025-11-04 02:28:30 +0300

    Draft version

--- goltsov.vadim/P3/main.cpp
+++ goltsov.vadim/P3/main.cpp
@@ -7,6 +7,8 @@ namespace goltsov
   long long * create(size_t rows, size_t cols);
   void destroy(long long * mtx, size_t created);
   void get_mtx(long long * mtx, size_t rows, size_t cols, std::istream & input);
+  bool LWRTRIMTX(long long * mtx, size_t n, size_t shift, size_t rows, size_t flag);
+  size_t CNTLOCMAX(long long * mtx, size_t rows, size_t cols);
 }
 
 int main(int argc, char ** argv)
@@ -42,7 +44,6 @@ int main(int argc, char ** argv)
   }
   //
 
-  char * outp = argv[3];
 
   //Получение матрицы
   std::ifstream input(argv[2]);
@@ -79,6 +80,67 @@ int main(int argc, char ** argv)
     }
   }
   //
+
+  //LWR-TRI-MTX
+  bool answear_1 = goltsov::LWRTRIMTX(num == 1 ? auto_mtx : dyn_mtx, rows < cols ? rows : cols, rows < cols ? (cols-rows) : (rows-cols), rows, rows < cols ? 0 : 1);
+  //
+  
+  //CNT-LOC-MAX
+  size_t answear_2 = goltsov::CNTLOCMAX(num == 1 ? auto_mtx : dyn_mtx, rows, cols);
+  //
+  
+  //Вывод результата
+  std::ofstream output(argv[3]);
+  output << "Expects output (return code 0): " << answear_1 << '\n';
+  output << "Expects output (return code 0): " << answear_2 << '\n';
+  //
+  
+  if (num == 2)
+  {
+    goltsov::destroy(dyn_mtx);
+  }
+}
+
+bool goltsov::LWRTRIMTX(long long * mtx, size_t n, size_t shift, size_t rows, size_t flag)
+{
+  size_t bad_rez = 0;
+  for (size_t sh = 0; sh <= shift; sh++)
+  {
+    for (size_t i = 0; i < n; ++i)
+    {
+      for (size_t j = i+1; j < n; ++ j)
+      {
+        if (!mtx[(i+sh*flag)*rows + j + sh*flag])
+        {
+          ++bad_rez;
+        }
+      }
+    }
+  }
+  return !(bad_rez == shift);
+}
+
+size_t goltsov::CNTLOCMAX(long long * mtx, size_t rows, size_t cols)
+{
+  size_t answear = 0;
+  if (rows <= 2 || cols <= 2)
+  {
+    return 0;
+  }
+  for (int i = 1; i < rows - 1; ++i)
+  {
+    for (int j = 1; j < cols - 1; ++j)
+    {
+      if (mtx[i*rows + j] > mtx[(i-1)*rows + j] && mtx[i*rows + j] > mtx[(i+1)*rows + j])
+      {
+        if (mtx[i*rows + j] > mtx[i*rows + j - 1] && mtx[i*rows + j] > mtx[i*rows + j + 1])
+        {
+          ++answear;
+        }
+      }
+    }
+  }
+  return answear;
 }
 
 long long * goltsov::create(size_t rows, size_t cols)
@@ -107,4 +169,3 @@ void goltsov::get_mtx(long long * mtx, size_t rows, size_t cols, std::istream & 
     }
   }
 }
-
