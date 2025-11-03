5fa9fab65a47212263aa033d2d60fcc0b05f6b36
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-03 17:40:29 +0300

    edit to CG try 2

--- zharov.danil/P3/main.cpp
+++ zharov.danil/P3/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char ** argv)
   if (argc < 4) {
     std::cerr << "Not enough arguments\n";
     return 1;
-  } 
+  }
   if (argc > 4) {
     std::cerr << "Too many arguments\n";
     return 1;
