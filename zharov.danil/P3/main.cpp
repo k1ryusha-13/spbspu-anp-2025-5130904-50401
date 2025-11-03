7e4c79e34c3fefef564287ed52e4a6bd3d90bc41
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-03 17:20:06 +0300

    added check args

--- zharov.danil/P3/main.cpp
+++ zharov.danil/P3/main.cpp
@@ -1,2 +1,38 @@
-int main()
-{}
+#include <iostream>
+
+bool isArgNum(char * arg);
+
+int main(int argc, char ** argv)
+{
+  if (argc < 4) {
+    std::cerr << "Not enough arguments\n";
+    return 1;
+  } 
+  if (argc > 4) {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+  if (! isArgNum(argv[1])) {
+    std::cerr << "First parameter is not a number\n";
+    return 1;
+  }
+  if ((argv[1][0] != '1' && argv[1][0] != '2') || argv[1][1] != '\0') {
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+  std::cout << "All is good\n";
+}
+
+bool isArgNum(char * arg)
+{
+  if (arg[0] == '\0') {
+    return false;
+  } else {
+    for (int i = 0; arg[i] != '\0'; ++i) {
+      if (arg[i] < '0' || arg[i] > '9') {
+        return false;
+      }
+    }
+  }
+  return true;
+}
