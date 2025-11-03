14607304a1d3e9ef77675ee0b5a4fac7c52add00
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-03 22:21:24 +0300

    Fixed args test

--- kuznetsov.petr/P3/main.cpp
+++ kuznetsov.petr/P3/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv)
   } else if (!kuz::isNumber(argv[1])) {
     std::cerr << "First parameter is not a number\n";
     return 1;
-  } else if (argv[1][0] != '1' && argv[1][0] != '2' && argv[1][0] == '\0') {
+  } else if ((argv[1][0] != '1' && argv[1][0] != '2') || argv[1][1] != '\0') {
     std::cerr << "First parameter is out of range\n";
     return 1;
   }
