34f71aea0d989ca3721f14e139dfaf7ae504a725
aaaa24 <chernov.arseniy@yandex.ru>
2025-11-04 20:15:51 +0300

    finished fll_inc_wav

--- chernov.arseniy/P3/main.cpp
+++ chernov.arseniy/P3/main.cpp
@@ -2,12 +2,12 @@
 #include <fstream>
 
 namespace chernov {
-  void matrix_input(std::istream & input, int ** mtx, size_t rows, size_t cols);
-  void matrix_input(std::istream & input, int (&mtx)[], size_t rows, size_t cols);
+  void matrix_input(std::istream & input, int * mtx, size_t rows, size_t cols);
+  // void matrix_input(std::istream & input, int (&mtx)[], size_t rows, size_t cols);
   void destroy(int ** mtx, size_t created);
   int ** create(size_t rows, size_t cols);
   bool isNumber(char * word);
-  void fll_inc_wav(std::ostream & output, int (&mtx)[], size_t rows, size_t cols);
+  void fll_inc_wav(std::ostream & output, int mtx[], size_t rows, size_t cols);
 }
 
 void chernov::matrix_input(std::istream & input, int ** mtx, size_t rows, size_t cols)
@@ -21,7 +21,7 @@ void chernov::matrix_input(std::istream & input, int ** mtx, size_t rows, size_t
 
 void chernov::matrix_input(std::istream & input, int (&mtx)[] , size_t rows, size_t cols)
 {
-  for (size_t i; i < rows * cols; ++i) {
+  for (size_t i = 0; i < rows * cols; ++i) {
     input >> mtx[i];
   }
 }
@@ -59,9 +59,37 @@ bool chernov::isNumber(char * word)
   return (word[0] != '\0');
 }
 
-void chernov::fll_inc_wav(std::ostream & output, int (&mtx)[], size_t rows, size_t cols)
+void chernov::fll_inc_wav(std::ostream & output, int mtx[], size_t rows, size_t cols)
 {
-  
+  size_t add = 1;
+  size_t x = 0, y = 0, count = 0;
+  while (count++ < rows * cols) {
+    mtx[cols * y + x] += static_cast<int>(add);
+    if (y == add - 1 && x != cols - add) {
+      ++x;
+    }
+    else if (x == cols - add && y != rows - add) {
+      ++y;
+    }
+    else if (y == rows - add && x != add - 1) {
+      --x;
+    }
+    else if (x == add - 1) {
+      if (y == add) {
+        add++;
+        x++;
+      }
+      else {
+        --y;
+      }
+    }
+  }
+
+  output << rows << " " << cols;
+  for (size_t i = 0; i < rows * cols; ++i) {
+    output << " " << mtx[i];
+  }
+  output << "\n";
 }
 
 int main(int argc, char ** argv)
@@ -81,6 +109,9 @@ int main(int argc, char ** argv)
   }
 
   std::ifstream input(argv[2]);
+  //std::istream& input = std::cin;
+  std::ofstream output(argv[3]);
+  //std::ostream& output = std::cout;
   size_t rows = 0, cols = 0;
   input >> rows >> cols;
   if (! input) {
@@ -95,7 +126,7 @@ int main(int argc, char ** argv)
       std::cerr << "Incorrect input\n";
       return 2;
     }
-
-
+    chernov::fll_inc_wav(output, matrix, rows, cols);
+    return 0;
   }
 }
