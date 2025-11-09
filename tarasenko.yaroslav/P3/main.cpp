af0960959eb912a5b6d4e983810ffdb16d5f9947
RapidMindd <YaroslavT007@yandex.ru>
2025-11-09 21:34:03 +0300

    basic file handling added

--- tarasenko.yaroslav/P3/main.cpp
+++ tarasenko.yaroslav/P3/main.cpp
@@ -1,45 +1,76 @@
 #include <iostream>
 #include <fstream>
 
-void input_processing(int argc, char ** argv)
+namespace tarasenko
 {
-  if (argc < 4)
+  size_t input_static(std::istream & in, int * arr, size_t n, size_t m)
   {
-    throw std::invalid_argument("Not enough arguments");
-  }
-  if (argc > 4)
-  {
-    throw std::invalid_argument("Too many arguments");
-  }
-  const char * first_arg = argv[1];
-  size_t i = 0;
-  while (first_arg[i] != '\0')
-  {
-    if (first_arg[i] > '9' || first_arg[i] < '0')
+    for (size_t i = 0; i < n * m; ++i)
     {
-      throw std::invalid_argument("First parameter is not a number");
+      in >> arr[i];
     }
-    i += 1;
+    return n * m;
   }
-  if (first_arg[0] != '1' && first_arg[0] != '2')
+
+  std::ostream & output(std::ostream & out, const int * a, size_t n, size_t m)
   {
-    throw std::invalid_argument("First parameter is out of range");
+    for (size_t i = 0; i < n * m; ++i)
+    {
+      out << a[i] << ' ';
+    }
+    out << '\n';
+    return out;
   }
-  if (first_arg[1] != '\0')
+
+  void input_processing(int argc, char ** argv)
   {
-    throw std::invalid_argument("First parameter is out of range");
+    if (argc < 4)
+    {
+      throw std::invalid_argument("Not enough arguments");
+    }
+    if (argc > 4)
+    {
+      throw std::invalid_argument("Too many arguments");
+    }
+    const char * first_arg = argv[1];
+    size_t i = 0;
+    while (first_arg[i] != '\0')
+    {
+      if (first_arg[i] > '9' || first_arg[i] < '0')
+      {
+        throw std::invalid_argument("First parameter is not a number");
+      }
+      i += 1;
+    }
+    if (first_arg[0] != '1' && first_arg[0] != '2')
+    {
+      throw std::invalid_argument("First parameter is out of range");
+    }
+    if (first_arg[1] != '\0')
+    {
+      throw std::invalid_argument("First parameter is out of range");
+    }
   }
+
 }
 
 int main(int argc, char ** argv)
 {
   try
   {
-    input_processing(argc, argv);
+    tarasenko::input_processing(argc, argv);
   }
   catch (const std::invalid_argument &e)
   {
     std::cerr << e.what() << '\n';
     return 1;
   }
+  std::ifstream input(argv[2]);
+  std::ofstream output(argv[3]);
+  size_t rows = 0, cols = 0;
+  input >> rows >> cols;
+  int arr[10000] = {};
+  tarasenko::input_static(input, arr, rows, cols);
+  tarasenko::output(output, arr, rows, cols);
+
 }
