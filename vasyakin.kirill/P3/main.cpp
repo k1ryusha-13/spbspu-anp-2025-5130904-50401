ea4f358dcc0d607fa1a0ec33c0f0d93c62a6f479
potomnazovy <skeleton39876@mail.ru>
2025-11-11 19:00:31 +0300

    function for output static matrix

--- vasyakin.kirill/P3/main.cpp
+++ vasyakin.kirill/P3/main.cpp
@@ -10,6 +10,18 @@ void destroy(int ** a, size_t created)
   }
   delete[] a;
 }
+void output_static(const int (* a) [100], size_t rows, size_t cols, std::ofstream & output)
+{
+  for (size_t i = 0; i < rows; ++i)
+  {
+    output << a[i][0];
+    for (size_t j = 1; j < cols; ++j)
+    {
+      output << ' ' << a[i][j];
+    }
+    output << '\n';
+  }
+}
 int main()
 {
 }
