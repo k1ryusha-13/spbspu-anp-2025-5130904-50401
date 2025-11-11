5a3bb3c1bfba225b3005a2736a2adc70d3bbbbc6
potomnazovy <skeleton39876@mail.ru>
2025-11-11 19:03:10 +0300

    function for output dynamic matrix

--- vasyakin.kirill/P3/main.cpp
+++ vasyakin.kirill/P3/main.cpp
@@ -22,6 +22,18 @@ void output_static(const int (* a) [100], size_t rows, size_t cols, std::ofstrea
     output << '\n';
   }
 }
+void output_dynamic(const int * const * a, size_t rows, size_t cols, std::ofstream & output)
+{
+  for (size_t i = 0; i < rows; ++i)
+  {
+    output << a[i][0];
+    for (size_t j = 1; j < cols; ++j)
+    {
+      output << ' ' << a[i][j];
+    }
+    output << '\n';
+  }
+}
 int main()
 {
 }
