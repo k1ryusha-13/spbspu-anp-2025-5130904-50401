68231d7575a593ba3d35376bdb535d6e8d5dc964
potomnazovy <skeleton39876@mail.ru>
2025-11-11 18:53:51 +0300

    function for destroy matrix

--- vasyakin.kirill/P3/main.cpp
+++ vasyakin.kirill/P3/main.cpp
@@ -2,6 +2,14 @@
 #include <cstddef>
 #include <limits>
 #include <fstream>
+void destroy(int ** a, size_t created)
+{
+  for (size_t i = 0; i < created; ++i)
+  {
+    delete[] a[i];
+  }
+  delete[] a;
+}
 int main()
 {
 }
