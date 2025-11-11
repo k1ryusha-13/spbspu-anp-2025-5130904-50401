53609ee1ea5bb56f4522f74df4b76544bcd66dc8
anyable <stupiranna@mail.ru>
2025-11-11 19:33:09 +0300

    made namespace stupir

--- stupir.anna/P3/main.cpp
+++ stupir.anna/P3/main.cpp
@@ -1,103 +1,106 @@
 #include <iostream>
 #include <fstream>
 
-bool checkAddSnail(size_t up, size_t down, size_t left, size_t right)
+namespace stupir
 {
-  return (up <= down && left <= right);
-}
-
-int * addSnail(int * arr, size_t rows, size_t cols)
-{
-  size_t sum = 1;
-  size_t left = 0; 
-  size_t right = cols - 1;
-  size_t up = 0;
-  size_t down = rows - 1;
-  while (checkAddSnail(up, down, left, right))
+  bool checkAddSnail(size_t up, size_t down, size_t left, size_t right)
   {
-    for (size_t i = down; i >= up; --i)
+    return (up <= down && left <= right);
+  }
+
+  int * addSnail(int * arr, size_t rows, size_t cols)
+  {
+    size_t sum = 1;
+    size_t left = 0; 
+    size_t right = cols - 1;
+    size_t up = 0;
+    size_t down = rows - 1;
+    while (checkAddSnail(up, down, left, right))
     {
-      if (i > down)
+      for (size_t i = down; i >= up; --i)
+      {
+        if (i > down)
+        {
+          break;
+        }
+        arr[cols * i + left] += sum;
+        sum++;
+      }
+      left++;
+      if (!checkAddSnail(up, down, left, right))
       {
         break;
       }
-      arr[cols * i + left] += sum;
-      sum++;
-    }
-    left++;
-    if (!checkAddSnail(up, down, left, right))
-    {
-      break;
-    }
-    for (size_t i = left; i < right + 1; ++i)
-    {
-      arr[up * cols + i] += sum;
-      sum++;
-    }
-    up++;
-    if (!checkAddSnail(up, down, left, right))
-    {
-      break;
-    }
-    for (size_t i = up; i < down + 1; ++i)
-    {
-      arr[cols * i + right] += sum;
-      sum++;
-    }
-    right--;
-    if (!checkAddSnail(up, down, left, right))
-    {
-      break;
-    }
-    for (size_t i = right; i >= left; --i)
-    {
-      if (i > right)
+      for (size_t i = left; i < right + 1; ++i)
+      {
+        arr[up * cols + i] += sum;
+        sum++;
+      }
+      up++;
+      if (!checkAddSnail(up, down, left, right))
       {
         break;
       }
-      arr[cols * down + i] += sum;
-      sum++;
+      for (size_t i = up; i < down + 1; ++i)
+      {
+        arr[cols * i + right] += sum;
+        sum++;
+      }
+      right--;
+      if (!checkAddSnail(up, down, left, right))
+      {
+        break;
+      }
+      for (size_t i = right; i >= left; --i)
+      {
+        if (i > right)
+        {
+          break;
+        }
+        arr[cols * down + i] += sum;
+        sum++;
+      }
+      down--;
     }
-    down--;
+
+    return arr;
   }
 
-  return arr;
-}
-
-int * create(int * arr, const char * firstArg, size_t rows, size_t cols)
-{
-  if(firstArg == "1")
+  int * create(int * arr, const char * firstArg, size_t rows, size_t cols)
   {
-    arr[rows * cols] = {};
-  }
-  else
-  {
-    arr = new int[rows * cols];
-  }
-  return arr;
-}
-
-void readArr(std::ifstream& input, size_t rows, size_t cols, int * arr)
-{
-  for (size_t i = 0; i < rows * cols; ++ i)
+    if(firstArg == "1")
     {
-      input >> arr[i];
+      arr[rows * cols] = {};
     }
-}
-
-void writeArr(std::ofstream& output, size_t rows, size_t cols, int * arr)
-{
-  if (!output.fail())
-  {
-    output << arr[0];
-    for (size_t i = 1; i < rows * cols; ++ i)
+    else
     {
-      output << " " << arr[i];
+      arr = new int[rows * cols];
     }
+    return arr;
   }
-  else
+
+  void readArr(std::ifstream& input, size_t rows, size_t cols, int * arr)
   {
-    std::cerr << "Сouldn't open the file for writing\n";
+    for (size_t i = 0; i < rows * cols; ++ i)
+      {
+        input >> arr[i];
+      }
+  }
+
+  void writeArr(std::ofstream& output, size_t rows, size_t cols, int * arr)
+  {
+    if (!output.fail())
+    {
+      output << arr[0];
+      for (size_t i = 1; i < rows * cols; ++ i)
+      {
+        output << " " << arr[i];
+      }
+    }
+    else
+    {
+      std::cerr << "Сouldn't open the file for writing\n";
+    }
   }
 }
 
@@ -151,10 +154,11 @@ int main(int argc, char ** argv)
   }
 
   int * arr = nullptr;
+  namespace stu = stupir;
   try
   {
-    arr = create(arr, firstArg, rows, cols);
-    readArr(input, rows, cols, arr);
+    arr = stu::create(arr, firstArg, rows, cols);
+    stu::readArr(input, rows, cols, arr);
     if (input.fail())
     {
       std::cerr << "Non-correct values of matrix elements\n";
@@ -167,11 +171,9 @@ int main(int argc, char ** argv)
     std::cerr << "Not enough memory\n";
     return 2;
   }
-
-  arr = addSnail(arr, rows, cols);
-  
+  arr = stu::addSnail(arr, rows, cols);
   std::ofstream output(thirdArg);
-  writeArr(output, rows, cols, arr);
+  stu::writeArr(output, rows, cols, arr);
 
   if (firstArg == "2")
   {
