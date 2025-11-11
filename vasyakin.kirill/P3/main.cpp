f112df0ba8eb8c8cddf8d6f9433fa71f385c5782
potomnazovy <skeleton39876@mail.ru>
2025-11-11 19:36:20 +0300

    check bad or wrong input and added command line arguments

--- vasyakin.kirill/P3/main.cpp
+++ vasyakin.kirill/P3/main.cpp
@@ -208,6 +208,30 @@ void vasyakin::spiral_dynamic(int ** a, size_t rows, size_t cols)
     }
   }
 }
-int main()
+int main(int argc, char ** argv)
 {
+  if (argc != 4)
+  {
+    std::cerr << "Error:" << " ";
+    if (argc < 4)
+    {
+      std::cerr << "Not enough arguments" << "\n";
+      return 1;
+    }
+    else
+    {
+      std::cerr << "Too many arguments" << "\n";
+      return 1;
+    }
+  }
+  if (*argv[1] != '1' && *argv[1] != '2')
+  {
+    std::cerr << "First parameter is out of range" << "\n";
+    return 1;
+  }
+  else if (argv[1][1] != '\0')
+  {
+    std::cerr << "First parameter is not number" << "\n";
+    return 1;
+  }
 }
