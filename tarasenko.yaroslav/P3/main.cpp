f2bddeefc7bf400af5d7734233877f4ce56fceba
RapidMindd <YaroslavT007@yandex.ru>
2025-11-13 12:10:04 +0300

    function for the task added

--- tarasenko.yaroslav/P3/main.cpp
+++ tarasenko.yaroslav/P3/main.cpp
@@ -3,11 +3,15 @@
 
 namespace tarasenko
 {
-  size_t input_static(std::istream & in, int * arr, size_t n, size_t m)
+  size_t input(std::istream & in, int * arr, size_t n, size_t m)
   {
     for (size_t i = 0; i < n * m; ++i)
     {
       in >> arr[i];
+      if (!in)
+      {
+        throw i;
+      }
     }
     return n * m;
   }
@@ -20,6 +24,50 @@ namespace tarasenko
     }
     out << '\n';
     return out;
+  }
+
+  size_t cnt_loc_extremum(int const * arr, int type, size_t n, size_t m)
+  // type 1 for max and 0 for min
+  {
+    if (n * m == 0)
+    {
+      return 0;
+    }
+    size_t count = 0;
+    for (size_t i = 1; i < n - 1; ++i)
+    {
+      for (size_t k = 1; k < m - 1; ++k)
+      {
+        bool is_extremum = true;
+        const int num = arr[i * m + k];
+        for (int near_i = -1; near_i < 2; ++near_i)
+        {
+          for (int near_k = -1; near_k < 2; ++near_k)
+          {
+            if (!(near_i == 0 && near_k == 0))
+            {
+              const int near_num = arr[(i + near_i) * m + (k + near_k)];
+              if (type)
+              {
+                if (num <= near_num)
+                {
+                  is_extremum = false;
+                }
+              }
+              else
+              {
+                if (num >= near_num)
+                {
+                  is_extremum = false;
+                }
+              }
+            }
+          }
+        }
+        count += (is_extremum ? 1 : 0);
+      }
+    }
+    return count;
   }
 
   void input_processing(int argc, char ** argv)
@@ -51,7 +99,6 @@ namespace tarasenko
       throw std::invalid_argument("First parameter is out of range");
     }
   }
-
 }
 
 int main(int argc, char ** argv)
@@ -69,8 +116,50 @@ int main(int argc, char ** argv)
   std::ofstream output(argv[3]);
   size_t rows = 0, cols = 0;
   input >> rows >> cols;
-  int arr[10000] = {};
-  tarasenko::input_static(input, arr, rows, cols);
-  tarasenko::output(output, arr, rows, cols);
-
+  if (!input)
+  {
+    std::cerr << "Incorrect file" << '\n';
+    return 2;
+  }
+  if (*argv[1] == '1')
+  {
+    int arr[10000] = {};
+    try
+    {
+      tarasenko::input(input, arr, rows, cols);
+    }
+    catch (size_t e)
+    {
+      std::cerr << "Managed to read "<< e << " numbers from file" << '\n';
+      return 2;
+    }
+    int max = tarasenko::cnt_loc_extremum(arr, 1, rows, cols);
+    int min = tarasenko::cnt_loc_extremum(arr, 0, rows, cols);
+    output << max << '\n';
+    output << min << '\n';
+  }
+  else
+  {
+    int * arr = reinterpret_cast< int * >(malloc(sizeof(int) * rows * cols));
+    if (!arr)
+    {
+      std::cerr << "failed to allocate memory" << '\n';
+      return 1;
+    }
+    try
+    {
+      tarasenko::input(input, arr, rows, cols);
+    }
+    catch (size_t e)
+    {
+      std::cerr << "Managed to read "<< e << " numbers from file" << '\n';
+      free(arr);
+      return 2;
+    }
+    int max = tarasenko::cnt_loc_extremum(arr, 1, rows, cols);
+    int min = tarasenko::cnt_loc_extremum(arr, 0, rows, cols);
+    output << max << '\n';
+    output << min << '\n';
+    free(arr);
+  }
 }
