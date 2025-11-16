a6fb70350a2fac388782d2b033910d096b3388bb
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-16 15:16:34 +0300

    If there are not enough elements to read the matrix, the reading stops.

--- kuznetsov.petr/P3/main.cpp
+++ kuznetsov.petr/P3/main.cpp
@@ -70,7 +70,7 @@ int kuznetsov::getCntColNsm(const int* mtx, size_t rows, size_t cols)
   int res = 0;
   for (size_t j = 0; j < cols; ++j) {
     bool repeats = false;
-    for (size_t i = 0; i < rows-1; ++i) {
+    for (size_t i = 0; i < rows - 1; ++i) {
       if (mtx[i * cols + j] == mtx[(i + 1) * cols + j]) {
         repeats = true;
         break;
@@ -87,18 +87,17 @@ int kuznetsov::getCntLocMax(const int* mtx, size_t rows, size_t cols)
     return 0;
   }
   int res = 0;
-  for (size_t j = 1; j < cols-1; ++j) {
-    for (size_t i = 1; i < rows-1; ++i) {
+  for (size_t j = 1; j < cols - 1; ++j) {
+    for (size_t i = 1; i < rows - 1; ++i) {
       int center = mtx[i * cols + j];
       bool isLocMax = true;
       for (int di = -1; di <= 1; ++di) {
         for (int dj = -1; dj <= 1; ++dj) {
           if (!(di == 0 && dj == 0)) {
-            isLocMax = isLocMax && (center > mtx[(i+di) * cols + j+dj]);
+            isLocMax = isLocMax && (center > mtx[(i + di) * cols + j + dj]);
           }
         }
       }
-
       res += isLocMax;
     }
   }
@@ -109,7 +108,7 @@ std::istream& kuznetsov::initMatr(std::istream& input, int* mtx, size_t rows, si
 {
   size_t c = 0;
   while (c < rows*cols) {
-    if (input >> mtx[c]) {
+    if (!(input >> mtx[c])) {
       break;
     }
     ++c;
