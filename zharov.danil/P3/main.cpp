4b98bf337a3a4d24ca0cb03d50781da30db7edca
AxellorWave <Danya.Zharov.2006@gmail.com>
2025-11-19 11:24:01 +0300

    try fix VALGRIND ERRORS

--- zharov.danil/P3/main.cpp
+++ zharov.danil/P3/main.cpp
@@ -118,6 +118,9 @@ size_t zharov::getCntColNsm(const int * mtx, size_t rows, size_t cols)
 void zharov::processMatrix(std::ifstream & input, int * matrix, size_t rows, size_t cols, const char * output_file)
 {
   zharov::createMatrix(input, matrix, rows, cols);
+  if (input.fail()) {
+    return;
+  }
   std::ofstream output(output_file);
   output << zharov::isUppTriMtx(matrix, rows, cols) << "\n";
   output << zharov::getCntColNsm(matrix, rows, cols) << "\n";
