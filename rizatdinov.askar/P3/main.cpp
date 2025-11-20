66990e7b28a23b4b9e8e0e1dfd9e73d018a971b3
Asgard <rizatdinov06@mail.ru>
2025-11-20 16:00:44 +0300

    The final touch

--- rizatdinov.askar/P3/main.cpp
+++ rizatdinov.askar/P3/main.cpp
@@ -5,121 +5,123 @@
 
 namespace rizatdinov
 {
-  bool willOverflow(int result, int digit, int multiplier);
-  int stringToInt(const char * string);
-  unsigned parseArgs(const char * const * argv, int argc);
+  unsigned parseArgs(int argc, char ** argv);
   void errorHandler(unsigned parse_args);
-  bool isLocalMax(const int * matrix, size_t cols, unsigned position);
-  unsigned countLocalMax(const int * matrix, size_t rows, size_t cols);
-  bool isTriangularMatrix(const int * matrix, size_t rows, size_t cols);
+  int * creat(size_t len, int number);
+  void initial(int * array, size_t len, std::ifstream & file);
+  bool willOverflow(int number, int digit, int multiplier);
+  int stringToInt(const char * string);
+  bool isLocalMax(const int * array, size_t cols, size_t position);
+  unsigned long countLocalMax(const int * array, size_t rows, size_t cols);
+  bool isLowerTriangular(const int * array, size_t rows, size_t cols);
 }
 
 int main(int argc, char ** argv)
 {
-  namespace riz = rizatdinov;
-  unsigned parse_args = riz::parseArgs(argv, argc);
-  if (parse_args)
-  {
-    riz::errorHandler(parse_args);
-    return 1;
+  unsigned parse_args = rizatdinov::parseArgs(argc, argv);
+  if (parse_args) {
+    rizatdinov::errorHandler(parse_args);
+    return parse_args;
   }
-  int number = riz::stringToInt(argv[1]);
+
   std::ifstream input(argv[2]);
+
   size_t rows = 0, cols = 0;
-  int * matrix = nullptr;
   input >> rows >> cols;
-  if (input.fail())
-  {
-    std::cerr << "fatal: failed to read the file\n";
+  if (!input) {
+    rizatdinov::errorHandler(2);
     return 2;
   }
-  int static_matrix[rows * cols];
-  if (number == 1)
-  {
-    matrix = static_matrix;
-  }
-  else
-  {
-    matrix = reinterpret_cast< int * >(malloc(sizeof(int) * rows * cols));
-    if (matrix == nullptr)
-    {
-      std::cerr << "Bad alloc\n";
-      return 3;
-    }
-  }
-  for (size_t i = 0; i < cols * rows; ++i)
-  {
-    input >> matrix[i];
-    if (input.fail())
-    {
-      if (number == 2)
-      {
-        free(matrix);
-      }
-      std::cerr << "fatal: failed to read the file\n";
-      return 2;
-    }
+
+  int * array = nullptr;
+  int number = rizatdinov::stringToInt(argv[1]);
+  try {
+    array = rizatdinov::creat(rows * cols, number);
+    rizatdinov::initial(array, rows * cols, input);
+  } catch(...) {
+    rizatdinov::errorHandler(2);
+    return 2;
   }
 
   input.close();
 
-  unsigned count_lmax = riz::countLocalMax(matrix, rows, cols);
-  bool is_triangular = riz::isTriangularMatrix(matrix, rows, cols);
-
   std::ofstream output(argv[3]);
-  output << "Expects output (retrun code 0): " << count_lmax;
-  output << ' ' << (is_triangular ? "true" : "false");
 
-  if (number == 2)
-  {
-    free(matrix);
+  size_t count_local_max = rizatdinov::countLocalMax(array, rows, cols);
+  bool is_lower_triangular = rizatdinov::isLowerTriangular(array, rows, cols);
+
+  output << "Expects output (return code 0): " << count_local_max << '\n';
+  output << "Expects output (return code 0): " << is_lower_triangular << '\n';
+
+  if (number == 2) {
+    free(array);
   }
 
   return 0;
 }
 
-bool rizatdinov::isLocalMax(const int * matrix, size_t cols, unsigned position)
+unsigned rizatdinov::parseArgs(int argc, char ** argv)
 {
-  bool result = matrix[position] > matrix[position - 1];
-  result = result && matrix[position] > matrix[position + 1];
-  result = result && matrix[position] > matrix[position - cols];
-  result = result && matrix[position] > matrix[position + cols];
+  if (argc != 4) {
+    return 1;
+  }
+
+  int num = 0;
+  try {
+    num = rizatdinov::stringToInt(argv[1]);
+  } catch (...) {
+    return 1;
+  }
+
+  std::ifstream file(argv[2]);
+
+  unsigned result = (num < 1 || num > 2) ? 1 : 0;
+  result = !file ? 2 : result;
 
   return result;
 }
 
-unsigned rizatdinov::countLocalMax(const int * matrix, size_t rows, size_t cols)
+void rizatdinov::errorHandler(unsigned parse_args)
 {
-  unsigned count = 0;
-  for (size_t i = cols + 1; i < rows * (cols - 1); ++i)
-  {
-    if (i % cols == 0)
-    {
-      i++;
-      continue;
-    }
-    count += rizatdinov::isLocalMax(matrix, cols, i);
+  switch (parse_args) {
+    case 1:
+      std::cerr << "fatal: invalid parametr\n";
+      break;
+    case 2:
+      std::cerr << "fatal: invalid file\n";
+      break;
   }
-  return count;
 }
 
-bool rizatdinov::isTriangularMatrix(const int * matrix, size_t rows, size_t cols)
+int * rizatdinov::creat(size_t len, int number)
 {
-  bool result = true;
-  for (size_t i = 0; i < rows; ++i)
-  {
-    for (size_t j = 0; j < cols; ++j)
-    {
-      result = i > j ? matrix[i * j + j] == 0 : true;
+  int * array = nullptr;
+  int static_array[10000] = {};
+  if (number == 1) {
+    array = static_array;
+  } else {
+    array = reinterpret_cast< int * >(malloc(sizeof(int) * len));
+    if (array == nullptr) {
+      throw std::bad_alloc();
     }
   }
-  return result;
+  return array;
+}
+
+void rizatdinov::initial(int * array, size_t len, std::ifstream & file)
+{
+  for (size_t i = 0; i < len; ++i) {
+    file >> array[i];
+  }
+  if (!file) {
+    throw std::invalid_argument("invalid argument");
+  }
 }
 
 bool rizatdinov::willOverflow(int number, int digit, int multiplier)
 {
-  int max_limit = std::numeric_limits<int>::max();
-  int min_limit = std::numeric_limits<int>::min();
+  int max_limit = std::numeric_limits< int >::max();
+  int min_limit = std::numeric_limits< int >::min();
 
   bool result = number < min_limit + digit * multiplier;
   result = result || (result > max_limit - digit * multiplier);
@@ -130,38 +132,33 @@ bool rizatdinov::willOverflow(int number, int digit, int multiplier)
 int rizatdinov::stringToInt(const char * string)
 {
   int result = 0;
-  int digit = 0;
   int multiplier = 1;
-  int count = 0;
-  int start_limit = 0;
   int is_negative = 1;
+  int start_limit = 0;
 
-  namespace riz = rizatdinov;
-
-  if (string[0] == '-')
-  {
+  if (string[0] == '-') {
     is_negative = -1;
     start_limit++;
-    count++;
   }
-  for (; string[count] != '\0'; ++count)
-  {
-    if (string[count] < '0' || string[count] > '9')
-    {
+
+  size_t len = start_limit;
+  for (; string[len] != '\0'; ++len) {
+    if (string[len] < '0' || string[len] > '9') {
       throw std::invalid_argument("invalid number");
     }
   }
-  if (count - start_limit > 10)
-  {
+
+  if (len - start_limit > 10) {
     throw std::overflow_error("overflow");
   }
-  for (int i = count - 1; i >= start_limit; --i)
-  {
-    digit = static_cast<int>(string[i]) - 48;
-    if (riz::willOverflow(result, digit, multiplier))
-    {
+
+  for (size_t i = len; i > 0; --i) {
+    int digit = static_cast< int >(string[i - 1] - 48);
+
+    if (rizatdinov::willOverflow(result, digit, multiplier)) {
       throw std::overflow_error("overflow");
     }
+
     result += digit * multiplier * is_negative;
     multiplier *= 10;
   }
@@ -169,41 +166,38 @@ int rizatdinov::stringToInt(const char * string)
   return result;
 }
 
-unsigned rizatdinov::parseArgs(const char * const * argv, int argc)
+bool rizatdinov::isLocalMax(const int * array, size_t cols, size_t position)
 {
-  int num = 0;
-  unsigned result = argc < 4 || argc > 4;
-  if (result)
-  {
-    return 1;
-  }
-  try
-  {
-    num = rizatdinov::stringToInt(argv[1]);
-  }
-  catch (...)
-  {
-    return 2;
-  }
-  std::ifstream input(argv[2]);
-  result = (num < 1 || num > 2) ? 2 : 0;
-  result = !input.is_open() ? 3 : result;
+  bool result = array[position] > array[position + 1];
+  result = result && array[position] > array[position - 1];
+  result = result && array[position] > array[position + cols];
+  result = result && array[position] > array[position - cols];
 
   return result;
 }
 
-void rizatdinov::errorHandler(unsigned parse_args)
+unsigned long rizatdinov::countLocalMax(const int * array, size_t rows, size_t cols)
 {
-  switch (parse_args)
-  {
-    case 1:
-      std::cerr << "fatal: invalid number of arguments\n";
-      break;
-    case 2:
-      std::cerr << "fatal: invalid parameter\n";
-      break;
-    case 3:
-      std::cerr << "fatal: cannot open file\n";
-      break;
+  size_t count = 0;
+  for (size_t i = 1; i + 1 < rows; ++i) {
+    for (size_t j = 1; j + 1 < cols; ++j) {
+      size_t position = i * cols + j;
+      count += rizatdinov::isLocalMax(array, cols, position);
+    }
   }
+  return count;
+}
+
+bool rizatdinov::isLowerTriangular(const int * array, size_t rows, size_t cols)
+{
+  bool result = false;
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = i + 1; j < cols; ++j) {
+      if (array[i * cols + j] != 0) {
+        return false;
+      }
+    }
+    result = true;
+  }
+  return result;
 }
