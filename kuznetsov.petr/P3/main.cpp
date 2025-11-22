3c0bd6f97d89442375b0ed7d64e077a10d108ee0
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-22 14:36:54 +0300

    hot fix

--- kuznetsov.petr/P3/main.cpp
+++ kuznetsov.petr/P3/main.cpp
@@ -106,10 +106,8 @@ int kuznetsov::getCntLocMax(const int* mtx, size_t rows, size_t cols)
 
 std::istream& kuznetsov::initMatr(std::istream& input, int* mtx, size_t rows, size_t cols)
 {
-  for (size_t c = 0; c < rows * cols; ++c) {
-    if (input >> mtx[c]) {
-      break;
-    }
+  for (size_t i = 0; input && i < rows * cols; ++i) {
+    input >> mtx[i];
   }
   return input;
 }
