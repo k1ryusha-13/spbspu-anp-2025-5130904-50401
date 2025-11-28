7ff0e6ab9f6f4bfec58027514caa7a47ea1cc4d7
kirusha-13 <kirillukashevich2007@gmail.com>
2025-11-28 20:20:11 +0300

    Matrix output

--- lukashevich.kirill/P3/main.cpp
+++ lukashevich.kirill/P3/main.cpp
@@ -1,2 +1,30 @@
-int main()
-{}
+#include <iostream>
+#include <cstddef>
+#include <limits>
+#include <fstream>
+
+namespace lukashevich
+{
+  std::ostream& outMtx(std::ostream& out, const int* matrix, size_t rows, size_t cols)
+  {
+    out << rows << " " << cols;
+    if (rows > 0 && cols > 0)
+    {
+      out << " ";
+      for (size_t i = 0; i < rows; ++i)
+      {
+        out << matrix[i * cols];
+        for (size_t j = 1; j < cols; ++j)
+        {
+          out << " " << matrix[i * cols + j];
+        }
+      }
+    }
+    out << "\n";
+    return out;
+  }
+
+  
+int main(int argc, char ** argv){
+
+}
