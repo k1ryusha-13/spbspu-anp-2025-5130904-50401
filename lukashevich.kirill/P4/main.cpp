0cebfb3fbc9f1fb3528f90ce781a91f9e4a2c86a
kirusha-13 <kirillukashevich2007@gmail.com>
2026-01-05 16:39:00 +0300

    Add removeLatinLetters and in/output

--- lukashevich.kirill/P4/main.cpp
+++ lukashevich.kirill/P4/main.cpp
@@ -38,9 +38,65 @@ namespace lukashevich {
     delete[] oldStr;
     return newStr;
   }
+
+  char* getLine(std::istream& in)
+  {
+    const bool isSkipws = in.flags() & std::ios_base::skipws;
+    if (isSkipws) {
+      in >> std::noskipws;
+    }
+
+    size_t i = 0;
+    size_t size = 8;
+    char sym = 0;
+    char* str = createStr(size);
+    if (!str) {
+      return nullptr;
+    }
+
+    while (in >> sym && sym != '\n') {
+      if (i == size - 1) {
+        str = updateStr(str, size);
+        if (!str) {
+          return nullptr;
+        }
+        size *= 2;
+      }
+      str[i++] = sym;
+    }
+    str[i++] = '\0';
+
+    if (isSkipws) {
+      in >> std::skipws;
+    }
+
+    return str;
+  }
+
+  int removeLatinLetters(const char* str, char* result, const int resultSize)
+  {
+    if (str == nullptr || result == nullptr || resultSize <= 0) {
+      return -1;
+    }
+
+    int pos = 0;
+    for (size_t i = 0; str[i] != '\0'; ++i) {
+      const unsigned char c = str[i];
+      if (!std::isalpha(c)) {
+        if (pos + 1 >= resultSize) {
+          return -1;
+        }
+        result[pos++] = str[i];
+      }
+    }
+
+    result[pos] = '\0';
+    return pos;
+  }
 }
 
 int main()
 {
+  namespace luk = lukashevich;
   return 0;
 }
